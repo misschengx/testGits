6f44b31610267e8659b09ac6461462d31fc01d30
chengx <1176680101@qq.com>
2021-08-01 14:41:37 +0800

    cgx  主干添加

--- HellowGit/src/main/java/com/cgx/git/demoGit.java
+++ HellowGit/src/main/java/com/cgx/git/demoGit.java
@@ -9,5 +9,6 @@ public class demoGit {
     public static void main(String[] args) {
         System.out.println("hellow git");
         System.out.println("创建分支");
+        System.out.println("主干添加");
     }
 }
