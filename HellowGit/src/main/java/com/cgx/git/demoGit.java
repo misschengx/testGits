9c6bcc18232349ee61183f9189346351e682ca97
chengx <1176680101@qq.com>
2021-08-01 13:20:47 +0800

    cgx

---
+++ HellowGit/src/main/java/com/cgx/git/demoGit.java
@@ -0,0 +1,13 @@
+package com.cgx.git;
+
+/**
+ * @author: CGX
+ * @description:
+ * @create: 2021-08-01 13:09
+ */
+public class demoGit {
+    public static void main(String[] args) {
+        System.out.println("hellow git");
+
+    }
+}
