d211e928402e1b5f8b830389f2cb43656f4b9fc4
misschengx <88266180+misschengx@users.noreply.github.com>
2021-08-02 22:23:50 +0800

    Update demoGit.java

--- HellowGit/src/main/java/com/cgx/git/demoGit.java
+++ HellowGit/src/main/java/com/cgx/git/demoGit.java
@@ -13,6 +13,7 @@ public class demoGit {
         System.out.println("分支添加2");
         System.out.println("金华改了代码");
         System.out.println("金华改了代码2");
+        System.out.println("金华改了代码5");
         System.out.println("金华改了代码4");
     }
 }
